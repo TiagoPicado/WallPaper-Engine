42071cf2db3cdb4ae62530499a1d166238331f96
Tiago Picado <tiago@nunopicado.com>
2020-12-17 23:26:29 +0000

    Script 24H

---
+++ 24H.js
@@ -0,0 +1,28 @@
+'use strict';
+/**
+ * @param {String} value (for property 'text')
+ */
+var lastString;
+var lastTimeTag;
+var stringIndex = 0;
+
+export function update() {
+  var today = new Date();
+  var h = checkTime(today.getHours());
+  var m = checkTime(today.getMinutes());
+  var s = checkTime(today.getSeconds());
+   
+  let newString =  h + ":" + m;
+  if (newString != lastString) {
+    lastString = newString;
+    thisLayer.text = newString.replace('$', engine.userProperties.name);
+  }
+}
+ 
+export function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  };  // add zero in front of numbers < 10
+  
+  return i;
+}
